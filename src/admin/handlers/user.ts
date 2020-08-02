0fd42e64f1ed989f112b84e98d71300e83eff085
Lam <daibanglam@gmail.com>
2020-08-02 16:36:10 +0700

    refactor: rename functions

--- src/admin/handlers/user.ts
+++ src/admin/handlers/user.ts
@@ -1,19 +1,7 @@
 import axios from 'axios';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-export const getUsers: APIGatewayProxyHandler = async (event, context) => {
+export const getUserList: APIGatewayProxyHandler = async (event, context) => {
   let response;
   try {
     const resp = await axios.get('https://www.metaweather.com/api/location/search', {
@@ -32,11 +20,7 @@ export const getUsers: APIGatewayProxyHandler = async (event, context) => {
   return response;
 };
 
-interface IdPath {
-  id: string;
-}
-
-export const getUser: APIGatewayProxyHandler = async (event, context) => {
+export const getUserDetail: APIGatewayProxyHandler = async (event, context) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify('123'),
