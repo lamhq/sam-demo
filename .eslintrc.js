f76e6c0953b3b8c9e49b710daf1723e53f31b988
Lam <daibanglam@gmail.com>
2020-08-04 09:03:32 +0700

    feat: update config for Nodejs project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,17 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    jest: true,
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
